09ef9ad30ec2eccce8fa7c36d9285e0b62583038
Ankush <alamba570@gmail.com>
2023-09-05 16:52:00 +0530

    minor changes

---
+++ app/src/main/java/com/geralt/linkedin_clone/Profile/EditProfileIntroActivity.java
@@ -0,0 +1,81 @@
+package com.geralt.linkedin_clone.Profile;
+
+import static com.geralt.linkedin_clone.Constants.USER_CONSTANT;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.geralt.linkedin_clone.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class EditProfileIntroActivity extends AppCompatActivity {
+
+    EditText editTextFirstName, editTextLastName, editTextHeadline, editTextPosition, editTextEducation, editTextLocation;
+    String stringUserName, stringUserImgUrl, stringUserLocation;
+    DatabaseReference ref;
+    FirebaseUser user;
+    TextView saveBtn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile_intro);
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        ref = FirebaseDatabase.getInstance().getReference().child(USER_CONSTANT).child(user.getUid());
+        editTextFirstName = findViewById(R.id.edit_first_name);
+        editTextLastName = findViewById(R.id.edit_last_name);
+        editTextHeadline = findViewById(R.id.edit_headline);
+        editTextPosition = findViewById(R.id.edit_position);
+        editTextEducation = findViewById(R.id.edit_education);
+        editTextLocation = findViewById(R.id.edit_location);
+
+        saveBtn = findViewById(R.id.save_btn);
+
+        Intent intent = getIntent();
+        stringUserName = intent.getStringExtra("user_name");
+        stringUserImgUrl = intent.getStringExtra("user_imgUrl");
+        stringUserLocation = intent.getStringExtra("user_location");
+
+
+        String[] split = stringUserName.split(" ");
+        editTextFirstName.setText(split[0]);
+        editTextLastName.setText(split[1]);
+
+        editTextLocation.setText(stringUserLocation);
+
+
+        saveBtn.setOnClickListener(v -> {
+            ref.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    Map<String, Object> map = new HashMap<>();
+                    map.put("education", editTextEducation.getText().toString());
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+
+            });
+        });
+
+    }
+}
